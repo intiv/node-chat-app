b89912608c71b6f72f26a74b3bdd610420b24755
Inti Velasquez <intiv@unitec.edu>
2018-07-20 18:51:47 -0600

    Styles and better user experience

--- public/js/index.js
+++ public/js/index.js
@@ -27,11 +27,12 @@ socket.on('newLocationMessage', function (msg) {
 
 jQuery('#message-form').on('submit', function(e){
     e.preventDefault();
+    let msgTextBox = jQuery('[name=message]');
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: msgTextBox.val()
     }, function() {
-
+        msgTextBox.val('');
     });
 });
 
@@ -40,13 +41,15 @@ locationButton.on('click', function() {
     if(!navigator.geolocation){
         return alert('Geolocation not supported by your browser');
     }
-
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
     navigator.geolocation.getCurrentPosition(function(position) {
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
+        locationButton.removeAttr('disabled').text('Send location');
     }, function(){
         alert('Unable to fetch location');
+        locationButton.removeAttr('disabled').text('Send location');
     });
 });
