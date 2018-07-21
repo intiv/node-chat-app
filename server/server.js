5d987e310c46f8e5d6038e6c8bfd488895bb5ee9
Inti Velasquez <intiv@unitec.edu>
2018-07-20 18:35:44 -0600

    Geolocation message

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
@@ -21,7 +21,7 @@ io.on('connection', (socket) => {
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat app'));
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined!'));
 
-    socket.on('createMessage', function(msg, callback){
+    socket.on('createMessage', (msg, callback) => {
         console.log('Message created', msg);
         // io.emit('newMessage', {
         //     from: msg.from,
@@ -32,6 +32,9 @@ io.on('connection', (socket) => {
         callback('This is from server');
     });
 
+    socket.on('createLocationMessage', (coords) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    })
     socket.on('disconnect', () => {
         console.log('User was disconnected');
     });
